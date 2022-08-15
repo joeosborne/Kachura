fb70361ac2ff39a2b6781fab83b3ec942f72b9d5
DESKTOP-7AG33JP\joe_p <joe_p_osborne@hotmail.com>
2022-08-15 13:29:33 +0100

    initial drag/drop component

--- boxing-rankings/UI/boxing-rankings-ng/src/app/app.module.ts
+++ boxing-rankings/UI/boxing-rankings-ng/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { EditRankingsComponent } from './admin/edit-rankings/edit-rankings.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditRankingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
