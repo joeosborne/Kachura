055c2291018269cfc3aae190aa26cc1cc67dc288
DESKTOP-7AG33JP\joe_p <joe_p_osborne@hotmail.com>
2022-08-17 10:32:16 +0100

    simple login with hardcoded data

--- boxing-rankings/UI/boxing-rankings-ng/src/app/admin/login/login.component.ts
+++ boxing-rankings/UI/boxing-rankings-ng/src/app/admin/login/login.component.ts
@@ -1,28 +1,31 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {Router} from "@angular/router";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   @Input() error: string | null = null;
 
   @Output() submitEM = new EventEmitter();
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   form: FormGroup = new FormGroup({
-    username: new FormControl(''),
+    email: new FormControl(''),
     password: new FormControl(''),
   });
 
   submit() {
-    if (this.form.valid) {
+    if (
+      this.form.valid &&
+      this.loginIsSucessful(this.form.value.email, this.form.value.password)
+    ) {
+      // todo: add user creds into LS here
       //this.router.navigate(['/dashboard', { email: this.profileForm.value.email }]);
       this.router.navigate(['/edit-rankings']);
 
@@ -31,5 +34,8 @@ export class LoginComponent implements OnInit {
     }
   }
 
-
+  // todo: replace..
+  private loginIsSucessful(email: string, password: string): boolean {
+    return email === 'joe@blah.com' && password === 'qqq';
+  }
 }
