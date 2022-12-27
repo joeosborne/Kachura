643f427473b64de91b098dbdeacf2014a4cdb787
joe_p <joe_p_osborne@hotmail.com>
2022-12-27 14:33:27 +0000

    Horizon - registration wip

--- horizon/ui/ng/src/app/register/register.component.ts
+++ horizon/ui/ng/src/app/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'horizon-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
+
+// todo: finish Register form and post to API layer
 export class RegisterComponent implements OnInit {
+  registrationForm: FormGroup = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: NonNullableFormBuilder) {
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
 }
--- horizon/ui/ng/tailwind.config.js
+++ horizon/ui/ng/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Mulish", "sans-serif"],
+        mono: ["Rokkitt", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
