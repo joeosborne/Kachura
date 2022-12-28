a3e91029475d2d24e5e85fd643a4319e43d69d56
joe_p <joe_p_osborne@hotmail.com>
2022-12-28 14:59:21 +0000

    css zen wip

--- tw-css-zen/tailwind.config.js
+++ tw-css-zen/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Libre Baskerville'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+      'julius': ['Julius Sans One', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'k-green': '#BCD5C7',
+        'k-design-selection': '#616857'
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
