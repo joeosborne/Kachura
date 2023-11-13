fc2d31c4e844180170a4dc92d92c439322d0fc51
joe_p <joe_p_osborne@hotmail.com>
2023-11-13 14:03:20 +0000

    ng 16 and 17 features spike - wip

--- spikes/Ng16And17/src/app/app.component.spec.ts
+++ spikes/Ng16And17/src/app/app.component.spec.ts
@@ -4,8 +4,10 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [AppComponent],
-}).compileComponents();
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngModToStandalone'`, () => {
+  it(`should have as title 'Ng16And17'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngModToStandalone');
+    expect(app.title).toEqual('Ng16And17');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngModToStandalone app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Ng16And17 app is running!');
   });
 });
--- spikes/ngModToStandalone/src/app/app.component.ts
+++ spikes/ngModToStandalone/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {DataSpinService} from "../data-spin.service";
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
 })
 export class AppComponent implements OnInit{
   title = 'ngModToStandalone';
