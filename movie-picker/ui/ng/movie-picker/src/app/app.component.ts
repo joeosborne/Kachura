108c0a0c39ac3e0dd9a1a0359b18c4f9e8d52c19
joe_p <joe_p_osborne@hotmail.com>
2023-12-28 15:29:17 +0000

    css spikes and initial movie quiz app prep

---
+++ movie-picker/ui/ng/movie-picker/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'movie-picker';
+}
+
+
+/* todo:
+For now, add media query for mobile first
+Add css variables
+PWA
+
+
+
+
+ */
