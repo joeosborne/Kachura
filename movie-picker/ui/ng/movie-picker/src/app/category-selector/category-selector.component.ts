bbce6221c76b497d12f1125fe6b779835259e092
joe_p <joe_p_osborne@hotmail.com>
2023-12-28 21:39:40 +0000

    movie picker progress

--- movie-picker/ui/ng/movie-picker/src/app/category-selector/category-selector.component.ts
+++ movie-picker/ui/ng/movie-picker/src/app/category-selector/category-selector.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieDataService } from '../movie-data.service';
 import { Category } from '../models/category.model';
+import {Router, ActivatedRoute, ParamMap, RouterOutlet} from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mp-category-selector',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './category-selector.component.html',
   styleUrl: './category-selector.component.css',
 })
 export class CategorySelectorComponent implements OnInit {
   categories: Category[] = [];
 
-  constructor(private movieDataService: MovieDataService) {}
+  constructor(
+    private movieDataService: MovieDataService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
   ngOnInit() {
     this.categories = this.movieDataService.movieData.categories;
     console.log(this.categories);
   }
 
-  onCategorySelected(category: Category)
-  {
-    console.log(category)
-    const movies = this.movieDataService.movieData.movies.filter(x=>x.categories.some(c=>c === category.id))
-    console.log('todo: show the following movies...')
-    console.log(movies)
+  onCategorySelected(category: Category) {
+    console.log(category);
 
+    this.router.navigate(['/category', { id: category.id }]);
+    //this.router.navigate(['/category']);
   }
 }
