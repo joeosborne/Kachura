de34f2f735af9d122dfefc6251ac11942a0f2847
joe_p <joe_p_osborne@hotmail.com>
2024-12-03 17:35:00 +0000

    coding test prep

---
+++ CodingTest.UnitTests/UnitTest1.cs
@@ -0,0 +1,403 @@
+namespace CodingTest.UnitTests
+{
+    // todo: split test classes..
+    [TestClass]
+    public class FindPairWithDictionaryTests    {
+
+            
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums = { 1, 4, 5, 7, 8, 9 };
+            int k = 10;
+
+            var result = CodingTests.CodingTest.FindPairWithDictionary(nums, k);
+
+            Assert.AreEqual((1, 9), result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int k = 9;
+
+            var result = CodingTests.CodingTest.FindPairWithDictionary(nums, k);
+
+            Assert.AreEqual((2, 7), result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int k = 12;
+
+            var result = CodingTests.CodingTest.FindPairWithDictionary(nums, k);
+
+            Assert.IsNull(result);
+
+        }
+    }
+
+    [TestClass]
+    public class FindFirstRepeatingCharacterTests
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "abcddef";
+            char expected = 'd';
+            
+
+            var result = CodingTests.CodingTest.FindFirstRepeatingCharacter(input);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "abcxxxxxdefx";
+            char expected = 'x';
+
+
+            var result = CodingTests.CodingTest.FindFirstRepeatingCharacter(input);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "fwkj£hrtze£jkl";
+            char expected = '£';
+
+
+            var result = CodingTests.CodingTest.FindFirstRepeatingCharacter(input);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4None()
+        {
+            string input = "abc^dexyz";
+
+
+            var result = CodingTests.CodingTest.FindFirstRepeatingCharacter(input);
+
+            Assert.IsNull(result);
+
+        }
+
+    }
+
+
+    [TestClass]
+    public class RemoveDuplicatesTests
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+                string input = "abcddef";
+            string expected = "abcdef";
+
+
+            var result = CodingTests.CodingTest.RemoveDuplicates(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "abcd£poyddle";
+            string expected = "abcd£poyle";
+
+
+            var result = CodingTests.CodingTest.RemoveDuplicates(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "qpwoeirutyalskdjfhqpalmcnxb";
+            string expected = "qpwoeirutyalskdjfhmcnxb";
+
+
+            var result = CodingTests.CodingTest.RemoveDuplicates(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+
+    [TestClass]
+    public class FindTheDuplicate
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class TreeDeothFirstSearch
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class MaximumSubarray
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class ReverseBinaryTree
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class ReverseLinkedList
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class PeakFinding
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class PalindromeLinkedList
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class LongestPossiblePalindrome
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class GetSubstringIndex
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class TreeBreadthFirstSearch
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class SortLinkedList
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class ValidBinarySearchTree
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class MinimumCostPathInMatrix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class BalancedBinaryTree
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+    [TestClass]
+    public class PathsInMatrix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "";
+            string expected = "";
+
+
+            var result = "x";// CodingTests.CodingTest.(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
---
+++ CodingTests/CodingTest.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodingTests
+{
+    public static class CodingTest
+    {
+
+        // Problem Statement
+        // Given an array of integers nums and an integer K, return the pair of numbers that add up to K.
+
+        // NOTE: Assumption for this test is that there is only one pair that adds up to the target number.
+        // TODO: Add a test case for multiple pairs that add up to the target number.
+        public static (int, int)? FindPairWithDictionary(int[] nums, int k)
+        {
+            Dictionary<int, int> complementMap = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = k - nums[i];
+
+                if (complementMap.ContainsKey(complement))
+                {
+                    return (complementMap[complement], nums[i]);
+
+                }
+                complementMap[nums[i]] = nums[i];
+
+            }
+
+
+
+            return null; // No pair found
+        }
+
+        public static char? FindFirstRepeatingCharacter(string input)
+        {
+            char? result = null;
+            HashSet<char> seen = new();
+
+            foreach(char c in input)
+            {
+                if (seen.Contains(c))
+                {
+                    return c;
+                }
+                seen.Add(c);
+            }
+
+            return result;
+        }
+
+
+        // todo: add to/refactor this and the tests for...
+        //Small Collections: HashSet or Distinct() (simple and efficient).
+        //Maintain Order: Use ToDictionary() or LINQ with Distinct().
+        //Custom Logic: Use a manual loop with a secondary collection.
+        // .. maybe separate methods with a list of strings and/or numbers
+        public static string RemoveDuplicates(string input)
+        {
+            HashSet<string> dedupe = new();
+            string result = "";
+            
+            foreach(char c in input)
+            {
+                dedupe.Add(c.ToString());
+            }
+
+            foreach (string c in dedupe)
+            {
+                result += c;
+            }
+
+            return result;
+
+        }
+
+
+        public static string FindTheDuplicate(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string TreeDeothFirstSearch(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string MaximumSubarray(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string ReverseBinaryTree(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string LongestSubstringWithoutRepeatingCharacters(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string ReverseLinkedList(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string PeakFinding(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string PalindromeLinkedList(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string LongestPossiblePalindrome(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string GetSubstringIndex(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string TreeBreadthFirstSearch(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string SortLinkedList(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string ValidBinarySearchTree(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string MinimumCostPathInMatrix(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string BalancedBinaryTree(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+        public static string PathsInMatrix(string input)
+        {
+            // todo: change method signature and implement
+            throw new NotImplementedException();
+
+        }
+
+
+        /*
+         * FindTheDuplicate
+         * TreeDeothFirstSearch
+         * MaximumSubarray
+         * ReverseBinaryTree
+         * LongestSubstringWithoutRepeatingCharacters
+         * ReverseLinkedList
+         * PeakFinding
+         * PalindromeLinkedList
+         * LongestPossiblePalindrome
+         * GetSubstringIndex
+         * TreeBreadthFirstSearch
+         * SortLinkedList
+         * ValidBinarySearchTree
+         * MinimumCostPathInMatrix
+         * BalancedBinaryTree
+         * PathsInMatrix
+         * 
+         * 
+         * Roman numerals - both ways
+         * fizzbuzz
+         * IsPalindrome
+         * 
+         */
+    }
+}
